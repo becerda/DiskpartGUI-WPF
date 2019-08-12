36c12dc12c3330b53d2c18065f21ee63a4d780ff
becerda <benjamin.cerda773@myci.csuci.edu>
2019-08-12 16:51:15 -0700

    Branch ProcessRunner 	- CMDProcess.cs 		- Initial class setup 			- Runs a cmd command 	- LabelProcess.cs 		- Initial class setup 			- Runs the label cmd command to rename a volume 	- ProcessRunner.cs 		- Initial class setup 			- Base class to help set up processes

---
+++ Processes/LabelProcess.cs
@@ -0,0 +1,29 @@
+﻿using DiskpartGUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskpartGUI.Processes
+{
+    static class LabelProcess
+    {
+
+        private static readonly string Error_Regex = @"Cannot change label.  This volume is write protected.";
+
+        public static ProcessExitCode RenameVolume(ref Volume v, string newlabel)
+        {
+            CMDProcess label = new CMDProcess("label");
+            label.AddArgument(v.DriveLetter + " " + newlabel);
+            if (label.Run() == ProcessExitCode.Ok)
+            {
+                if (label.TestOutput(Error_Regex))
+                    return ProcessExitCode.Error;
+                v.Label = newlabel;
+                return ProcessExitCode.Ok;
+            }
+            return ProcessExitCode.Error;
+        }
+    }
+}
---
+++ Processes/ProcessRunner.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DiskpartGUI.Processes
+{
+
+
+    enum ProcessExitCode
+    {
+        Ok,
+        Error
+    }
+
+    class ProcessRunner
+    {
+
+        public Process Process { get; set; }
+
+        public string StdOutput { get; private set; }
+
+        public string StdError { get; private set; }
+
+        public ProcessExitCode ExitCode { get; set; }
+
+        public ProcessRunner(string processname)
+        {
+            Process = new Process();
+            Process.StartInfo.FileName = processname;
+            Process.StartInfo.RedirectStandardInput = true;
+            Process.StartInfo.RedirectStandardOutput = true;
+            Process.StartInfo.RedirectStandardError = true;
+            Process.StartInfo.CreateNoWindow = true;
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.StartInfo.Arguments = @"/C ";
+        }
+
+        public ProcessRunner(string processname, string arguments) : this(processname)
+        {
+            AddArgument(arguments);
+        }
+
+        public void AddArgument(string argument)
+        {
+            Process.StartInfo.Arguments += @argument + " ";
+        }
+
+        public void ClearArguments()
+        {
+            Process.StartInfo.Arguments = @"/C";
+        }
+
+        public ProcessExitCode Run()
+        {
+            Process.StartInfo.Arguments.TrimEnd(' ');
+            if (Process.Start())
+            {
+                StdOutput = Process.StandardOutput.ReadToEnd();
+                StdError = Process.StandardError.ReadToEnd();
+                Process.WaitForExit();
+                return ProcessExitCode.Ok;
+            }
+
+            return ProcessExitCode.Error;
+        }
+
+        public bool TestOutput(string regex)
+        {
+            Match result = Regex.Match(StdOutput, regex);
+            return result.Success;
+        }
+    }
+}
