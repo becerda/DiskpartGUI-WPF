28dd6f14a388a458536344dd4ce4e02e1cd51772
becerda <benjamin.cerda773@myci.csuci.edu>
2019-08-15 15:32:13 -0700

    Branch ButtonCommands 	- BaseCommand.cs 		- Deleted replaced with RelayCommand.cs 	- CommandApply.cs 		- Deleted replaced with RelayCommand.cs 	- CommandCancel.cs 		- Deleted replaced with RelayCommand.cs 	- CommandEject.cs 		- Deleted replaced with RelayCommand.cs 	- CommandRefresh.cs 		- Deleted replaced with RelayCommand.cs 	- CommandRename.cs 		- Deleted replaced with RelayCommand.cs 	- RelayCommand.cs 		- Initial class setup 			- Relays a command back to the view model 	- ApplyCancelViewModel.cs 		- Initial class setup 			- Inherits from ClosablePropertyChangedViewModel 			- Apply command and method 			- CanApply check 			- Cancel command and method 	- BaseViewModel.cs 		- Deleted replaced with ClosablePropertyChangedViewModel.cs 	- ClosablePropertyChangedViewModel.cs 		- Initial class setup 			- Implements INotifyPropertyChanged 			- Implements IClosable 	- MainWindowViewModel.cs 		- Replaced CommandEject with RelayCommand 		- Replaced CommandRefresh with RelayCommand 		- Replaced CommandRename with RelayCommand 		- Added ChangeMountState for relay execute 		- Added CanChangeMountState for relay canexecute 		- Added IsSelectedVolumeValid for relay canexecute 		- Removed Cancel method 		- Removed Apply method 	- RenameWindowViewModel.cs 		- Changed inheritance to ApplyCancelViewModel 		- Removed CanApply property, is implemented through inheritance 		- Removed UpdateCanApply method, is implemented through inheritance 		- Changed confirm message label to uppercase 		- Implemented abstract methods 	- MainWindow.xaml 		- Removed command parameters 	- RenamWindow.xaml 		- Removed command parameters

---
+++ ViewModels/ApplyCancelViewModel.cs
@@ -0,0 +1,48 @@
+﻿using DiskpartGUI.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskpartGUI.ViewModels
+{
+    abstract class ApplyCancelViewModel : ClosablePropertyChangedViewModel
+    {
+        /// <summary>
+        /// The RelayCommand for Apply to be used with bindings
+        /// </summary>
+        public RelayCommand ApplyCommand { get; private set; }
+
+        /// <summary>
+        /// The RelayCommand for Cancel to be used with bindings
+        /// </summary>
+        public RelayCommand CancelCommand { get; private set; }
+
+        /// <summary>
+        /// Sets up Apply and Cancel Commands
+        /// </summary>
+        public ApplyCancelViewModel() : base()
+        {
+            ApplyCommand = new RelayCommand(Apply, CanApply);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        /// <summary>
+        /// Apply method to be called
+        /// </summary>
+        public abstract void Apply();
+
+        /// <summary>
+        /// The check to enable Apply method
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public abstract bool CanApply(object o);
+
+        /// <summary>
+        /// Cancel method to be called
+        /// </summary>
+        public abstract void Cancel();
+    }
+}
---
+++ ViewModels/ClosablePropertyChangedViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DiskpartGUI.Commands;
+using DiskpartGUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskpartGUI.ViewModels
+{
+    abstract class ClosablePropertyChangedViewModel : INotifyPropertyChanged, IClosable
+    {
+        /// <summary>
+        /// RequestClose event handler
+        /// </summary>
+        public event EventHandler<EventArgs> RequestClose;
+
+        /// <summary>
+        /// Property changed event handler
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The RelayCommand to close a window
+        /// </summary>
+        public RelayCommand CloseWindowCommand { get; private set; }
+
+        /// <summary>
+        /// Constructor assigning CloseCommand
+        /// </summary>
+        public ClosablePropertyChangedViewModel()
+        {
+            
+            CloseWindowCommand = new RelayCommand(RequestWindowClose);
+        }
+
+        /// <summary>
+        /// Requests the window to close
+        /// </summary>
+        protected void RequestWindowClose()
+        {
+            RequestClose?.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// Notifies the property changed event
+        /// </summary>
+        /// <param name="property">The property that is being changed</param>
+        protected void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
