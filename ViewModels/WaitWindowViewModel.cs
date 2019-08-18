a357b0b81114690ef72198a9c092a4a72e5dff38
becerda <benjamin.cerda773@myci.csuci.edu>
2019-08-18 16:57:11 -0700

    Branch WaitWindow 	- Spinner.xaml 		- Initial UserControl setup 			- The spinning animation used for waiting 	- Spinner.xaml.cs 		- Code-behind 			- Initializes components 	- WaitWindowViewModel.cs 		- Inital class setup 			- Can set a message to a spinner animation 	- WaitWindow.xaml 		- Inital window setup 			- Shows a message and a spinner animation 	- WaitWindow.xaml.cs 		- Code-behind 			- Initializes components

---
+++ ViewModels/WaitWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskpartGUI.ViewModels
+{
+    class WaitWindowViewModel
+    {
+        private string message;
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            private set
+            {
+                message = value;
+            }
+        }
+
+        public WaitWindowViewModel(string message)
+        {
+            Message = message;
+        }
+
+    }
+}
